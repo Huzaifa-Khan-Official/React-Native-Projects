09e6d16a319cf01f34e2fa4aec150115561292cc
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-09-27 23:54:33 +0500

    working with Dice Component

--- RollTheDice/App.tsx
+++ RollTheDice/App.tsx
@@ -1,7 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
+import React, { PropsWithChildren } from 'react'
 
-const App = () => {
+import DiceOne from "./assets/One.png"
+import DiceTwo from "./assets/Two.png"
+import DiceThree from "./assets/Three.png"
+import DiceFour from "./assets/Four.png"
+import DiceFive from "./assets/Five.png"
+import DiceSix from "./assets/Six.png"
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
+
+const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  )
+}
+
+const App = (): JSX.Element => {
   return (
     <View>
       <Text>App</Text>
@@ -11,4 +30,29 @@ const App = () => {
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+})
