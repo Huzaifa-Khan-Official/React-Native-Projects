385a05de1eed86903d9ff21e2f6e3bf7e1ed128b
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-10-05 16:27:47 +0500

    ticTacToe almost Done

--- TicTacToe/src/App.tsx
+++ TicTacToe/src/App.tsx
@@ -1,10 +1,104 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Snackbar from 'react-native-snackbar'
+import Icons from './components/Icons'
 
-const App = () => {
+const App = (): JSX.Element => {
+  const [isCross, setIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>("");
+  const [gameState, setGameState] = useState(new Array(9).fill("empty", 0, 9));
+
+  const relodeGame = () => {
+    setIsCross(false);
+    setGameWinner("");
+    setGameState(new Array(9).fill("empty", 0, 9));
+  }
+
+  const checkIsWinner = () => {
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== "empty"
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`)
+    } else if (
+      gameState[3] === gameState[4] &&
+      gameState[3] === gameState[5] &&
+      gameState[3] !== "empty"
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`)
+    } else if (
+      gameState[6] === gameState[7] &&
+      gameState[6] === gameState[8] &&
+      gameState[6] !== "empty"
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`)
+    } else if (
+      gameState[0] === gameState[3] &&
+      gameState[0] === gameState[6] &&
+      gameState[0] !== "empty"
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`)
+    } else if (
+      gameState[1] === gameState[4] &&
+      gameState[1] === gameState[7] &&
+      gameState[1] !== "empty"
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`)
+    } else if (
+      gameState[2] === gameState[5] &&
+      gameState[2] === gameState[8] &&
+      gameState[2] !== "empty"
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`)
+    } else if (
+      gameState[0] === gameState[4] &&
+      gameState[0] === gameState[8] &&
+      gameState[0] !== "empty"
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`)
+    } else if (
+      gameState[2] === gameState[4] &&
+      gameState[2] === gameState[6] &&
+      gameState[2] !== "empty"
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`)
+    } else if (
+      !gameState.includes("empty", 0)
+    ) {
+      setGameWinner("It's a draw... ⌛️")
+    }
+  }
+
+  const onChangeItem = (itemNumber: number) => {
+    if (gameWinner) {
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: "#000000",
+        textColor: "#FFFFFF"
+      });
+    }
+
+    if (gameState[itemNumber] === "empty") {
+      gameState[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      return Snackbar.show({
+        text: "Already marked! ��",
+        backgroundColor: "red",
+        textColor: "#FFFFFF"
+      })
+    }
+
+    checkIsWinner();
+  }
+
   return (
     <View>
-      <Text>Start working on TicTacToe</Text>
+      <StatusBar />
+      <View>
+        <Text>Start working on TicTacToe</Text>
+      </View>
     </View>
   )
 }
