329cdca5e62157dff55be4de7e5bddd84438a655
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-10-09 19:09:50 +0500

    create music Player service

---
+++ SpotifyClone/src/constant.ts
@@ -0,0 +1,58 @@
+import { Track } from "react-native-track-player";
+
+export const playListData: Track[] = [
+    {
+        id: 1,
+        title: '"Leila" - Violin Cover',
+        artist: 'Jah Khalib',
+        album: 'Leila',
+        artwork:
+            'https://i.ytimg.com/vi/4nVxrhKDS5k/maxresdefault.jpg',
+        url: require('./assets/audio/one.mp3'),
+    },
+    {
+        id: 2,
+        title: 'Mehram',
+        artist: 'Sachet-Parampara',
+        album: 'Jersey',
+        artwork:
+            'https://i.ytimg.com/vi/ImEloUnWex8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAFcKHnNSM-3GxZd_66kUy4JDwhEQ',
+        url: require('./assets/audio/two.mp3'),
+    },
+    {
+        id: 3,
+        title: 'Title Track',
+        artist: 'Arijit Singh, Parampara Tandon',
+        album: 'Pal Pal Dil Ke Paas',
+        artwork:
+            'https://c.saavncdn.com/328/Pal-Pal-Dil-Ke-Paas-Hindi-2019-20200420150444-500x500.jpg',
+        url: require('./assets/audio/three.mp3'),
+    },
+    {
+        id: 4,
+        title: 'Middle Of The Night ( Slowed+Reverb )',
+        artist: 'Elley Duhé',
+        album: 'Middle Of The Night',
+        artwork:
+            'https://i.ytimg.com/vi/YMQeFK4R3U4/sddefault.jpg',
+        url: require('./assets/audio/four.mp3'),
+    },
+    {
+        id: 5,
+        title: 'Nachange Saari Raat Full Song',
+        artist: 'Meet Bros, feat. Neeraj Shridhar & Tulsi Kumar. ',
+        album: 'JUNOONIYAT',
+        artwork:
+            'https://i.ytimg.com/vi/rxMmistOjCA/maxresdefault.jpg',
+        url: require('./assets/audio/five.mp3'),
+    },
+    {
+        id: 6,
+        title: 'Tu Itni Khoobsurat Hai [ Slowed Reverb ]',
+        artist: 'Jubin Nautiyal & Prakriti Kakar.',
+        album: 'Barkhaa',
+        artwork:
+            'https://i.ytimg.com/vi/PKIlSv9yMTY/maxresdefault.jpg',
+        url: require('./assets/audio/five.mp3'),
+    },
+];
