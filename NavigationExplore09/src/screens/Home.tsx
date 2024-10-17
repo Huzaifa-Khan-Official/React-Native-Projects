e12850a0192bb21288ab6bf54d94d53948faa432
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-10-17 23:22:09 +0500

    working on home screen

--- NavigationExplore09/src/screens/Home.tsx
+++ NavigationExplore09/src/screens/Home.tsx
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types'
+import { RootStackParamList } from '../App'
 
-const Home = () => {
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">
+
+const Home = ({ navigation }: HomeProps) => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text style={styles.smallText}>Home Screen</Text>
+            <Button
+                title='Go to details'
+                // onPress={() => navigation.navigate("Details", {
+                //   productId: "86"
+                // })}
+
+                // onPress={() => navigation.navigate("Details")}
+
+                onPress={() => navigation.push('Details', {
+                    productId: "86"
+                })}
+
+            />
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    smallText: {
+        color: "#000000"
+    }
+})
