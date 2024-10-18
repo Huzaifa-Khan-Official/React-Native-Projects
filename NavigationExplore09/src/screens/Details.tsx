9027e54135fed08aa55264755cb46e4b23b3f309
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-10-18 19:15:58 +0500

    React Native Navigator Configuration done

--- NavigationExplore09/src/screens/Details.tsx
+++ NavigationExplore09/src/screens/Details.tsx
@@ -11,4 +11,13 @@ const Details = () => {
 
 export default Details
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  smallText: {
+    color: "#000000"
+  }
+})
