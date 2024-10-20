295e706d1b48df7b9323031706d28cf7eb509fa3
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-10-20 19:53:41 +0500

    Project done

--- NavigationExplore09/src/screens/Home.tsx
+++ NavigationExplore09/src/screens/Home.tsx
@@ -1,26 +1,37 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types'
+
+// Navigation Stuff
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../App'
+
+// Component Stuff
+import ProductItem from '../components/ProductItem'
+import Separator from '../components/Separator'
+
+// Data
+import { PRODUCTS_LIST } from '../data/constants'
 
 type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">
 
 const Home = ({ navigation }: HomeProps) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.smallText}>Home Screen</Text>
-            <Button
-                title='Go to details'
-                // onPress={() => navigation.navigate("Details", {
-                //   productId: "86"
-                // })}
-
-                // onPress={() => navigation.navigate("Details")}
-
-                // onPress={() => navigation.push('Details', {
-                //     productId: "86"
-                // })}
-
+            <FlatList
+                data={PRODUCTS_LIST}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={Separator}
+                renderItem={({ item }) => (
+                    <Pressable
+                        onPress={() => {
+                            navigation.navigate("Details", {
+                                product: item
+                            })
+                        }}
+                    >
+                        <ProductItem product={item} />
+                    </Pressable>
+                )}
             />
         </View>
     )
@@ -31,10 +42,10 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+
+        padding: 12,
+        backgroundColor: '#FFFFFF',
     },
-    smallText: {
-        color: "#000000"
-    }
 })
